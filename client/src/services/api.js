df7a84152089e8e7330f9437f1617e3a29707910
Shubhamsharma1522 <shubhamsharma.in1522@gmail.com>
2024-02-06 12:38:40 +0530

    change server api

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URI = "http://localhost:8000";
+const API_URI = "https://gmail-clone-eta-olive.vercel.app";
 
 const API_GMAIL = async (serviceUrlObject, requestData = {}, type) => {
   const { params, urlParams, ...body } = requestData;
